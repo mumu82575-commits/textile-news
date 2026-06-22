7addfed0f63876cbc459c4b3afb08784ea3fba8f
mumu82575-commits <mumu82575@gmail.com>
2026-06-22 12:23:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,37 +2,58 @@ import feedparser
 from openpyxl import Workbook
 from datetime import datetime
 
+sources = [
 
-rss_url = "https://rss.nytimes.com/services/xml/rss/nyt/World.xml"
+("国际", "NYTimes",
+"https://rss.nytimes.com/services/xml/rss/nyt/World.xml"),
 
-feed = feedparser.parse(rss_url)
+("国际", "BBC",
+"https://feeds.bbci.co.uk/news/world/rss.xml")
 
+]
 
 wb = Workbook()
-
 ws = wb.active
+ws.title = "日报"
 
-ws.title = "新闻"
-
-
-ws.append(["日期", "标题", "来源"])
+ws.append([
+"日期",
+"分类",
+"标题",
+"来源"
+])
 
 
 today = datetime.now().strftime("%Y-%m-%d")
 
 
-for item in feed.entries[:10]:
+for category,name,url in sources:
 
-    ws.append([
-        today,
-        item.title,
-        "NYTimes"
-    ])
+    try:
+
+        feed = feedparser.parse(url)
+
+        for item in feed.entries[:10]:
+
+            ws.append([
+
+                today,
+
+                category,
+
+                item.title,
+
+                name
+
+            ])
+
+    except:
+
+        pass
 
 
 filename = f"日报_{today}.xlsx"
 
-
 wb.save(filename)
 
-print("Excel生成成功")
+print("日报生成成功")
